b31d0944ccd05463bd8bb397cbcae61500b7a239
Stephen Xia <xia.stephen@gmail.com>
2023-11-10 16:53:42 -0500

    Flashcard Class

--- flashcard-app/src/App.js
+++ flashcard-app/src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
+import Flashcard from "./Flashcard";
 import './App.css';
+import ".styles.scss";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Flaschard App
-        </h1>
-      </header>
+    <div className="container">
+      <div className="row h-100">
+        <div class="col d-flex flex-column flex-md-row justify-content-around align-items-center">
+          {cards.map((card) => (
+            <Flashcard key={card.id} card={card} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
--- flashcard-app/src/Flashcard.js
+++ flashcard-app/src/Flashcard.js
@@ -1,15 +1,34 @@
-function Flashcard() {
+import { useState } from "react"; 
+import cn from "classnames";
+
+function Flashcard({ card }) {
+  const [showBack, setShowBack] = useState(false); 
+
+  function handleClick() { 
+    if (card.variant === "click") { 
+      setShowBack(!showBack); 
+    } 
+  } 
+
   return (
-    <div className="flip-card-outer">
-      <div className="flip-card-inner">
+    <div
+      className="flip-card-outer"
+      onClick={handleClick} 
+    >
+      <div
+        className={cn("flip-card-inner", {
+          showBack, 
+          "hover-trigger": card.variant === "hover"
+        })}
+      >
         <div className="card front">
           <div className="card-body d-flex justify-content-center align-items-center">
-            <p className="card-text fs-1 fw-bold">Front</p>
+            <p className="card-text fs-1 fw-bold">{card.front}</p>
           </div>
         </div>
         <div className="card back">
           <div className="card-body d-flex justify-content-center align-items-center">
-            <p className="card-text fs-1 fw-bold">Back</p>
+            <p className="card-text fs-1 fw-bold">{card.back}</p>
           </div>
         </div>
       </div>
